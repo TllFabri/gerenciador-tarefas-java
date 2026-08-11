f4f588b6a998e798b5f87c89a7fd2fd9d081fcb5
TllFabri <thiagobarrosfabri07@gmail.com>
2026-08-10 21:29:37 -0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Jogo eldenRing = new Jogo("Elden Ring", "PC", "10/08/2026", 10, "Muito difícil");
