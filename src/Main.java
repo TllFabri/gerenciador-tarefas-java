b063f9257926256f5492d1be45f8303b40986e1e
TllFabri <thiagobarrosfabri07@gmail.com>
2026-08-10 21:26:35 -0300

    main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Jogo eldenRing = new Jogo("Elden Ring", "PC", "10/08/2026", 10, "Muito difícil");
+
+        System.out.println(eldenRing.getNome());
+        System.out.println(eldenRing.getPlataforma());
+        System.out.println(eldenRing.getNota());
+    }
+}
